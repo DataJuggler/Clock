de5a9e732ca14cd0112ba43e41c17876e387dfdb
DataJuggler <corby@datajuggler.com>
2023-07-16 08:58:26 -0500

    Add project files.

---
+++ Clock/ClockForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Clock
+{
+    partial class ClockForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ClockForm));
+            Timer = new System.Windows.Forms.Timer(components);
+            DateLabel = new Label();
+            TimeLabel = new Label();
+            SuspendLayout();
+            // 
+            // Timer
+            // 
+            Timer.Enabled = true;
+            Timer.Interval = 12000;
+            Timer.Tick += Timer_Tick;
+            // 
+            // DateLabel
+            // 
+            DateLabel.Dock = DockStyle.Bottom;
+            DateLabel.Font = new Font("Verdana", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            DateLabel.ForeColor = Color.Firebrick;
+            DateLabel.Location = new Point(0, 74);
+            DateLabel.Name = "DateLabel";
+            DateLabel.Size = new Size(344, 48);
+            DateLabel.TabIndex = 1;
+            DateLabel.Text = "Saturday October 16, 2023";
+            DateLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // TimeLabel
+            // 
+            TimeLabel.Dock = DockStyle.Fill;
+            TimeLabel.Font = new Font("Bookman Old Style", 48F, FontStyle.Regular, GraphicsUnit.Point);
+            TimeLabel.ForeColor = Color.Firebrick;
+            TimeLabel.Location = new Point(0, 0);
+            TimeLabel.Name = "TimeLabel";
+            TimeLabel.Size = new Size(344, 74);
+            TimeLabel.TabIndex = 2;
+            TimeLabel.Text = "9:43 PM";
+            TimeLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // ClockForm
+            // 
+            AutoScaleMode = AutoScaleMode.None;
+            BackColor = Color.Black;
+            ClientSize = new Size(344, 122);
+            Controls.Add(TimeLabel);
+            Controls.Add(DateLabel);
+            Font = new Font("Verdana", 48F, FontStyle.Regular, GraphicsUnit.Point);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Location = new Point(800, 600);
+            Name = "ClockForm";
+            StartPosition = FormStartPosition.Manual;
+            Text = "Clock";
+            TopMost = true;
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer Timer;
+        private Label DateLabel;
+        private Label TimeLabel;
+    }
+}
---
+++ Clock/ClockForm.cs
@@ -0,0 +1,67 @@
+
+
+#region using statements
+
+
+#endregion
+
+namespace Clock
+{
+
+    #region class ClockForm
+    /// <summary>
+    /// This class is a big font clock because I can't read Windows 11 clock, and Windows 11 clock doesn't open when clicked on.
+    /// </summary>
+    public partial class ClockForm : Form
+    {
+
+        #region Private Variables
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Create a new instance of a 'ClockForm' object.
+        /// </summary>
+        public ClockForm()
+        {
+            // Create Controls
+            InitializeComponent();
+
+            // setup the timer
+            Timer.Enabled = true;
+            TimeLabel.ForeColor = Color.Firebrick;
+            TimeLabel.Dock = DockStyle.Fill;
+            TimeLabel.TextAlign = ContentAlignment.MiddleCenter;
+
+            // Set the time at start up
+            TimeLabel.Text = DateTime.Now.ToShortTimeString();
+
+            // Update the date
+            DateLabel.Text = DateTime.Now.DayOfWeek + " " + DateTime.Now.ToShortDateString();
+        }
+        #endregion
+
+        #region Events
+
+        #region Timer_Tick(object sender, EventArgs e)
+        /// <summary>
+        /// event is fired when Timer _ Tick
+        /// </summary>
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+
+            // Update the time
+            TimeLabel.Text = now.ToShortTimeString();
+
+            // Update the date
+            DateLabel.Text = now.DayOfWeek + " " + now.ToShortDateString();
+        }
+        #endregion
+
+        #endregion
+
+    }
+    #endregion
+
+}
